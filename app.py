dadc0bcd9944c47b5a428f004292d3d6a88f88cb
amakzy <ahmadamakxy128@gmail.com>
2026-03-26 11:33:25 +0100

    Update API URL for prediction service

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_URL = "http://127.0.0.1:8007/predict"
+API_URL = "https://test-wgisin1x.b4a.run/predict"
 
 st.set_page_config(page_title="Fraud Detection System", layout="wide")
 st.title("Credit Card Fraud Detection Portal")
